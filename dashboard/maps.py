dc31929022ab2be7d9cc19d7726a883cdf8be7d2
Béatrice Picard <beatricepicard@Beatrices-MacBook-Air.local>
2025-03-21 15:09:24 +0100

    Changed titles in maps

--- dashboard/maps.py
+++ dashboard/maps.py
@@ -16,6 +16,7 @@ def continent_map(connection, continent, date):
     query_continent = f"SELECT * FROM new_complete WHERE Continent = '{continent}' AND Date = '{date}'" 
     df = pd.read_sql(query_continent, connection)
     df = remove_countries(df)
+    df = df[df["ActivePerPop"].notna() & (df["ActivePerPop"] >= 0)]
     lower_continent = continent.lower()
     max_value = min(df["ActivePerPop"].max(), 0.0035)
 
@@ -24,7 +25,7 @@ def continent_map(connection, continent, date):
                         locationmode="country names", title= f"Active Cases in {continent}",
                         hover_name = "Country", range_color=[0, max_value])
     fig.update_traces(hovertemplate="<b>%{hovertext}</b><br>Active Cases: %{z:.5%}")
-    fig.update_layout(coloraxis_colorbar=dict(title="Active Cases (%)", tickformat=".1%"))
+    fig.update_layout(coloraxis_colorbar=dict(title="Active Cases (%)", tickformat=".2%"))
     st.plotly_chart(fig)
     if continent == "Europe":
         st.markdown("**Note:** Sweden has been left out, because the data appears to be inaccurate/incomplete.")
@@ -40,10 +41,10 @@ def world_map(connection, date):
 
     fig = px.choropleth(df, scope="world", color="ActivePerPop",
                         color_continuous_scale= 'ylgnbu', locations="Country",
-                        locationmode="country names", title= f"Active Cases in the whole world",
+                        locationmode="country names", title= f"Active Cases on selected date",
                         hover_name = "Country", range_color=[0, max_value])
     fig.update_traces(hovertemplate="<b>%{hovertext}</b><br>Active Cases: %{z:.5%}")
-    fig.update_layout(coloraxis_colorbar=dict(title="Active Cases (%)", tickformat=".1%"))
+    fig.update_layout(coloraxis_colorbar=dict(title="Active Cases (%)", tickformat=".2%"))
     st.plotly_chart(fig)
     st.markdown("**Note:** Some countries might have been left out, because the data appears to be inaccurate/incomplete.")
     return
